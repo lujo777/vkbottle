b0bc85b03206c280c632d0b209dbea164fe41cc6
timoniq <52574917+timoniq@users.noreply.github.com>
2019-08-02 11:24:00 +0300

    v0.12 Utils Initial commit
    - keyboard poor code
    + keyboard docs

--- vkbottle/__init__.py
+++ vkbottle/__init__.py
@@ -5,44 +5,11 @@ from random import randint
 from datetime import datetime
 from .utils import Utils
 
-def _keyboard(self, pattern, one_time=False):
-    rows = pattern
-    row = rows[0]
-    buttons = list()
-    for row in rows:
-        row_buttons = list()
-        button = row[0]
-        for button in row:
-            row_buttons.append(dict(
-                action=dict(
-                    type="text" if 'type' not in button else button['type'],
-                    label=button['text'],
-                    payload=json.dumps("" if 'payload' not in button else button['payload'])
-                ),
-                color="default" if 'color' not in button else button['color']
-            )
-            )
-        buttons.append(row_buttons)
-
-    keyboard = str(json.dumps(
-        dict(
-            one_time=one_time,
-            buttons=buttons
-        ),
-        ensure_ascii=False
-    ).encode('utf-8').decode('utf-8'))
-
-    return keyboard
-
 
 # The main
 class Bot:
     _processor = {}
     _undefined_func = (lambda *args: print('Add to your on-message file an on-message-undefined decorator'))
-
-    def log_debug(self, text):
-        if self.debug is True:
-            print("[\x1b[34mVK Bottle\x1b[0m] " + text.replace('#', str(datetime.now().time())) + "\x1b[0m")
 
     def __init__(self, token, group_id, rps_delay=0, debug=False):
         self.session = vk_api.VkApi(token=token,)
@@ -55,7 +22,7 @@ class Bot:
 
     def process_message(self, text: str, obj):
         answer = AnswerObject(obj, self.session, self.group_id)
-        self.log_debug('\x1b[31;1m-> MESSAGE FROM {} TEXT "{}" TIME #'.format(obj['peer_id'], obj['text']))
+        self.utils('\x1b[31;1m-> MESSAGE FROM {} TEXT "{}" TIME #'.format(obj['peer_id'], obj['text']))
         if text in self._processor:
             self._processor[text](answer)
             self.utils(
@@ -83,13 +50,13 @@ class Bot:
         return decorator
 
     def run(self, wait=15):
-        self.log_debug('Found {} message decorators'.format(len(self._processor.keys())))
+        self.utils('Found {} message decorators'.format(len(self._processor.keys())))
         longpoll = self.session.method(
             'groups.getLongPollServer',
             {"group_id": self.group_id}
             )
         ts = longpoll['ts']
-        self.log_debug('Started listening longpoll...')
+        self.utils('Started listening longpoll...')
         while True:
             try:
                 url = f"{longpoll['server']}?act=a_check&key={longpoll['key']}&ts={ts}&wait={wait}"
@@ -99,7 +66,7 @@ class Bot:
                         obj = event['updates'][0]['object']
                         self.process_message(obj['text'], obj)
             except requests.ConnectTimeout:
-                self.log_debug('Request Connect Timeout! Reloading longpoll..')
+                self.utils.warn('Request Connect Timeout! Reloading longpoll..')
             except Exception as e:
                 print(e)
             try:
@@ -156,3 +123,36 @@ class AnswerObject:
         if 'keyboard' in request:
             request['keyboard'] = _keyboard(self, keyboard)
         return self.method('messages', 'send', request)
+
+
+def _keyboard(pattern, one_time=False):
+    """Simple keyboard constructor
+    :param pattern: Keyboard simple pattern, check github readme
+    :param one_time: Should keyboard be hidden after first use?
+    :return: VK Api Keyboard JSON
+    """
+    rows = pattern
+    buttons = list()
+    for row in rows:
+        row_buttons = list()
+        for button in row:
+            row_buttons.append(dict(
+                action=dict(
+                    type="text" if 'type' not in button else button['type'],
+                    label=button['text'],
+                    payload=json.dumps("" if 'payload' not in button else button['payload'])
+                ),
+                color="default" if 'color' not in button else button['color']
+            )
+            )
+        buttons.append(row_buttons)
+
+    keyboard = str(json.dumps(
+        dict(
+            one_time=one_time,
+            buttons=buttons
+        ),
+        ensure_ascii=False
+    ).encode('utf-8').decode('utf-8'))
+
+    return keyboard
--- vkbottle/utils.py
+++ vkbottle/utils.py
@@ -3,9 +3,15 @@ from time import time
 
 
 class Utils:
+    """Coloring class, engine of all debug messages and warns
+    """
     def __init__(self, debug):
         self.debug = debug
 
     def __call__(self, text):
         if self.debug is True:
             print("[\x1b[34mVK Bottle\x1b[0m] " + re.sub('#', str(time()), text) + "\x1b[0m")
+
+    def warn(self, text):
+        if self.debug is True:
+            print("[\x1b[34mVK Bottle WARN\x1b[0m] \x1b[31;1m" + re.sub('#', str(time()), text) + "\x1b[0m")
