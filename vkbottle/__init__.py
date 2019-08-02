49c9fe70041d4a9d59d8f2d85d04ef30461d5644
timoniq <52574917+timoniq@users.noreply.github.com>
2019-08-02 21:45:08 +0300

    v0.12 New events
    + events module
    - poor code
    - 3.6 vs 3.7 time troubles

--- vkbottle/__init__.py
+++ vkbottle/__init__.py
@@ -1,141 +1,44 @@
 import vk_api
 import requests
 import json
-import ujson
-import datetime
-import re
-
-from random import getrandbits, choice
-from enum import Enum
+from random import randint
+from .utils import Utils
+from .events import Events
 
 
-
-class JSONType(Enum):
-    STANDART = 'json'
-    FAST = 'ujson'
-
-class Bot:
-    _processor = {}
-    _undefined_func = (lambda *args: print('Add to your on-message file an on-message-undefined decorator'))
-
-    def _keyboard(self, pattern, one_time=False):
-        rows = pattern
-        buttons = list()
-        for row in rows:
-            row_buttons = list()
-            for button in row:
-                if self.json_type == JSONType.STANDART:
-                    row_buttons.append(dict(
-                        action=dict(
-                            type="text" if 'type' not in button else button['type'],
-                            label=button['text'],
-                            payload=json.dumps("" if 'payload' not in button else button['payload'])
-                        ),
-                        color="default" if 'color' not in button else button['color']
-                    )
-                    )
-                else:
-                    row_buttons.append(dict(
-                        action=dict(
-                            type="text" if 'type' not in button else button['type'],
-                            label=button['text'],
-                            payload=ujson.dumps("" if 'payload' not in button else button['payload'])
-                        ),
-                        color="default" if 'color' not in button else button['color']
-                    )
-                    )
-            buttons.append(row_buttons)
-        if self.json_type == JSONType.STANDART:
-            keyboard = str(json.dumps(
-                dict(
-                    one_time=one_time,
-                    buttons=buttons
-                ),
-                ensure_ascii=False
-            ).encode('utf-8').decode('utf-8'))
-        else:
-            keyboard = str(ujson.dumps(
-                dict(
-                    one_time=one_time,
-                    buttons=buttons
-                ),
-                ensure_ascii=False
-            ).encode('utf-8').decode('utf-8'))
-
-        return keyboard
-
-    def log_debug(self, text):
-        if self.debug is True:
-            print("[\x1b[34mVK Bottle\x1b[0m] " + re.sub('#', str(datetime.datetime.now()), text) + "\x1b[0m")
-
-    def __init__(self, token, group_id, json_type=JSONType.STANDART, rps_delay=0, debug=False):
-        self.session = vk_api.VkApi(token=token)
+# The main
+class Bot(Events):
+    def __init__(self, token, group_id, rps_delay=0, debug=False):
+        self.session = vk_api.VkApi(token=token,)
         self.session._auth_token()
         self.session.RPS_DELAY = rps_delay
-        self.json_type = json_type
         self.group_id = group_id
         self.debug = debug
-        self.log_debug('Bot was authorised successfully')
-        if json_type == JSONType.STANDART:
-            self.log_debug('Using a standart(slow) method of json parsing.')
-        if json_type == JSONType.FAST:
-            self.log_debug('Using a fast method of json parsing.')
-
-    def process_message(self, text: str, obj):
-        answer = MessageAnswer(obj, self.session, self.group_id)
-        self.log_debug('\x1b[31;1m-> MESSAGE FROM {} TEXT "{}" TIME #'.format(obj['peer_id'], obj['text']))
-        if text in self._processor:
-            self._processor[text](answer)
-            self.log_debug(
-                'New message compiled with decorator <\x1b[35m{}\x1b[0m> (from: {})'.format(
-                    self._processor[text].__name__, obj['peer_id']
-                ))
-        else:
-            self.log_debug(
-                'New message compile decorator was not found. '\
-                'Compiled with decorator \x1b[35m[on-message-undefined]\x1b[0m (from: {})'.format(
-                    obj['peer_id']
-                ))
-            self._undefined_func(answer)
-
-    def on_message(self, text):
-        def decorator(func):
-            self._processor[text] = func
-            return func
-        return decorator
-
-    def onAddToChat(self, peer_id):
-        def decorator(func):
-            self._processor[peer_id] = func
-            return func
-        return decorator
-
-    def on_message_undefined(self):
-        def decorator(func):
-            self._undefined_func = func
-            return func
-        return decorator
-
-    event = ''  # plz don`t touch that
+        self.utils = Utils(debug)
+        self.utils('Bot was authorised successfully')
 
     def run(self, wait=15):
-        self.log_debug('Found {} message decorators'.format(len(self._processor.keys())))
+        self.utils('Found {} message decorators'.format(len(self._processor_message.keys())))
         longpoll = self.session.method(
             'groups.getLongPollServer',
             {"group_id": self.group_id}
             )
         ts = longpoll['ts']
-        self.log_debug('Started listening longpoll...')
+        self.utils('Started listening longpoll...')
         while True:
             try:
                 url = f"{longpoll['server']}?act=a_check&key={longpoll['key']}&ts={ts}&wait={wait}"
                 event = requests.post(url).json()
+                print(event)
                 if event['updates']:
+                    obj = event['updates'][0]['object']
+                    print(self._events)
                     if event['updates'][0]['type'] == 'message_new':
-                        obj = event['updates'][0]['object']
                         self.process_message(obj['text'], obj)
+                    if event['updates'][0]['type'] in self._events:
+                        self.process_event(event)
             except requests.ConnectTimeout:
-                self.log_debug('Request Connect Timeout! Reloading longpoll..')
+                self.utils.warn('Request Connect Timeout! Reloading longpoll..')
             except Exception as e:
                 print(e)
             try:
@@ -147,17 +50,42 @@ class Bot:
             except Exception as E:
                 print(str(E))
 
+    def process_message(self, text: str, obj):
+        answer = AnswerObject(obj, self.session, self.group_id)
+        self.utils('\x1b[31;1m-> MESSAGE FROM {} TEXT "{}" TIME #'.format(obj['peer_id'], obj['text']))
+        if text in self._processor_message:
+            self._processor_message[text](answer)
+            self.utils(
+                'New message compiled with decorator <\x1b[35m{}\x1b[0m> (from: {})'.format(
+                    self._processor_message[text].__name__, obj['peer_id']
+                ))
+        else:
+            self.utils(
+                'New message compile decorator was not found. '\
+                'Compiled with decorator \x1b[35m[on-message-undefined]\x1b[0m (from: {})'.format(
+                    obj['peer_id']
+                ))
+            self._undefined_message_func(answer)
+
+    def process_event(self, event):
+        self.utils('\x1b[31;1m-> NEW EVENT FROM {} TYPE "{}" TIME #'.format(event['updates'][0]['object']['user_id'], event['updates'][0]['type']))
+        event_compile = True
+        for rule in self._events[event['updates'][0]['type']]['rule']:
+            event_compile = False if event['updates'][0]['object'].get(rule[0], 'undefined') != rule[1] else True
+        if event_compile:
+            answer = AnswerObject(event['updates'][0]['object'], self.session, self.group_id)
+            self.utils('* EVENT RULES => TRUE. COMPILING EVENT')
+            self._events[event['updates'][0]['type']]['call'](answer)
+
     def method(self, group, method, params):
         response = self.session.method(f"{group}.{method}", params)
         return response
 
 
-class MessageAnswer:
+class AnswerObject:
     def __init__(self, obj, session: vk_api.VkApi, group_id):
         self.obj = obj
-        self.peer_id = obj["peer_id"]
-        self.attachment = obj["attachments"]
-        self.fwd_message = obj["fwd_messages"]
+        self.peer_id = obj["peer_id"] if 'peer_id' in obj else obj['user_id']
         self.session = session
         self.group_id = group_id
 
@@ -171,12 +99,12 @@ class MessageAnswer:
             keyboard=keyboard,
             attachment=attachment,
             peer_id=self.peer_id,
-            random_id=getrandbits(31) * choice([-1, 1]),
+            random_id=randint(1, 2e5),
             sticker_id=sticker
         )
         request = {k: v for k, v in request.items() if v is not None}
         if 'keyboard' in request:
-            request['keyboard'] = Bot._keyboard(self, keyboard)
+            request['keyboard'] = _keyboard(self, keyboard)
         return self.method('messages', 'send', request)
 
     def send(self, peer_id, text, attachment=None, keyboard=None, sticker=None):
@@ -185,11 +113,43 @@ class MessageAnswer:
             keyboard=keyboard,
             attachment=attachment,
             peer_id=peer_id,
-            random_id=getrandbits(31) * choice([-1, 1]),
+            random_id=randint(1, 2e5),
             sticker_id=sticker
         )
         request = {k: v for k, v in request.items() if v is not None}
         if 'keyboard' in request:
-            request['keyboard'] = Bot._keyboard(self, keyboard)
+            request['keyboard'] = _keyboard(self, keyboard)
         return self.method('messages', 'send', request)
 
+
+def _keyboard(pattern, one_time=False):
+    """Simple keyboard constructor
+    :param pattern: Keyboard simple pattern, check github readme
+    :param one_time: Should keyboard be hidden after first use?
+    :return: VK Api Keyboard JSON
+    """
+    rows = pattern
+    buttons = list()
+    for row in rows:
+        row_buttons = list()
+        for button in row:
+            row_buttons.append(dict(
+                action=dict(
+                    type="text" if 'type' not in button else button['type'],
+                    label=button['text'],
+                    payload=json.dumps("" if 'payload' not in button else button['payload'])
+                ),
+                color="default" if 'color' not in button else button['color']
+            )
+            )
+        buttons.append(row_buttons)
+
+    keyboard = str(json.dumps(
+        dict(
+            one_time=one_time,
+            buttons=buttons
+        ),
+        ensure_ascii=False
+    ).encode('utf-8').decode('utf-8'))
+
+    return keyboard
---
+++ vkbottle/events.py
@@ -0,0 +1,38 @@
+from vkbottle.vkbottle.utils import Utils
+
+
+class Events:
+    _processor_message = {}
+    _processor_message_chat = {}
+    _undefined_message_func = (lambda *args: print('Add to your on-message file an on-message-undefined decorator'))
+    _events = {}
+
+    def on_message(self, text):
+        def decorator(func):
+            self._processor_message[text] = func
+            return func
+        return decorator
+
+    def on_message_undefined(self):
+        def decorator(func):
+            self._undefined_message_func = func
+            return func
+        return decorator
+
+    def on_message_chat(self, text):
+        def decorator(func):
+            self._processor_message_chat[text] = func
+            return func
+        return decorator
+
+    def on_group_join(self, join_type='join'):
+        def decorator(func):
+            self._events['group_join'] = {'call': func, 'rule': [['join_type', join_type]]}
+            return func
+        return decorator
+
+    def on_group_leave(self, yourself=True):
+        def decorator(func):
+            self._events['group_leave'] = {'call': func, 'rule': [['self', int(yourself)]]}
+            return func
+        return decorator
