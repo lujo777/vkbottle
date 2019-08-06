fbb25dcf30cb02f7c07964508d37155adf9775f4
newpotsdev <newpotsdev@gmail.com>
2019-08-06 09:58:28 +0200

    some changes
    - poor code
    + requirements.txt

--- examples/simple-answer.py
+++ examples/simple-answer.py
@@ -1,13 +1,13 @@
 from vkbottle import Bot, AnswerObject
 
-bot = Bot('my-token', 1, debug=True, asyncio=True)
-admin_id = 1  # VK Group admin ID
+bot = Bot('35c3149d2eb58d902cde288564e58309d73bb736dff1913a70b1f0172769b0cbaf414c9cb6a65e6f6f461', 181909496, debug=True, asyncio=True)
+admin_id = 168656722  # VK Group admin ID
 
 
 @bot.on_message('Hi')
 def hi(answer: AnswerObject):  # Type container is needed for syntax highlighting
     a = answer('Hi my friend!')
-    answer('Oh sorry! You are not my friend!')
+    answer('Oh sorry! {user:first_name} You are not my friend!')
     bot.api.messages.delete(a, delete_for_all=True)
 
 
@@ -17,12 +17,7 @@ def goodbye(answer: AnswerObject):
     bot.api.messages.send(admin_id, 'User said goodbye to bot :(')
 
 
-@bot.on_message_undefined()
-def undefined(answer: AnswerObject):
-    answer('I didnt understand you :|')
-
-
-@bot.on_group_join()
+@bot.on_group_join(join_type='accepted')
 def on_join(answer: AnswerObject):
     answer('Woof! Thanks for this!')
 
