2e4b89ab5ffd7e7889730c4836627b30c94e7cfe
timoniq <52574917+timoniq@users.noreply.github.com>
2019-08-09 18:35:29 +0300

    v.PyPl Initial commit of pypl
    + license
    + setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'vkbottle',
+  packages = ['vkbottle'],
+  version = '0.1',
+  license='MIT',
+  description = 'New bot-creating repo with options control like in the flask!',
+  author = 'Arseniy Timonik',
+  author_email = 'timonik.bss@gmail.com',
+  url = 'https://github.com/timoniq/vkbottle',
+  download_url = 'https://github.com/timoniq/vkbottle/archive/v0.1.tar.gz',
+  keywords = ['vk', 'vkontakte', 'vk-api', 'vk-bot', 'vkbottle', 'vk-bottle'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
